0d2b95eff200801ed7e42e03795bb7ee79f247d9
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-04-29 12:35:47 +0100

    changed name of notePlayer to NotePlayer

--- Assets/Scripts/ProceduralMusicSystem/notePlayer.cs
+++ Assets/Scripts/ProceduralMusicSystem/notePlayer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class notePlayer : MonoBehaviour
+public class NotePlayer : MonoBehaviour
 {
 
     // un-optimized version
