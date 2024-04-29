02b89cc33f0dae14e460cf63fcf7fda1573d0531
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-04-29 14:24:37 +0100

    ProceduralSound separated into another class

---
+++ Assets/Scripts/ProceduralMusicSystem/ProceduralSound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralSound : MonoBehaviour
+{
+
+    public NoteToFreq.Note note; //the note to be played
+
+    public float volume; //the volume
+
+    public int timeToPlayMili; //time for the note to play in milliseconds
+
+    public int channel; //a channel that'll play the music ( a pair of NotePlayer and an AudioSource, that is in the NotePlayer)
+
+    public int timeToWaitForPlay; //when are we supposed to play this sound in relation to the previous sound played
+
+
+}
