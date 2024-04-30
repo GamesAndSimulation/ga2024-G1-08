3e3f4141c7181cae1ca3cc178f2c225bba04c448
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-04-30 18:15:34 +0100

    Changed Procedural Sound starting values

--- Assets/Scripts/ProceduralMusicSystem/ProceduralSound.cs
+++ Assets/Scripts/ProceduralMusicSystem/ProceduralSound.cs
@@ -9,13 +9,10 @@ public class ProceduralSound
 {
 
     [SerializeField]
-    public float volume; //the volume multiplier in relation to the rest of the music
+    public float volume = 1; //the volume multiplier in relation to the rest of the music
 
     [SerializeField]
     public int channel; //a channel that'll play the music ( a pair of NotePlayer and an AudioSource, that is in the NotePlayer)
-
-    [SerializeField]
-    public float waitTime; //time to wait for the note, in fractions
 
     [SerializeField]
     public NoteToFreq.Note note; //the note to be played
@@ -23,10 +20,10 @@ public class ProceduralSound
     
     //the time to play the note is defined by a fracti
     [SerializeField]
-    public int playTimeNumerator; 
+    public int playTimeNumerator = 1; 
 
     [SerializeField]
-    public int playTimeDenumerator; 
+    public int playTimeDenumerator = 1; 
 
 
     [SerializeField]
