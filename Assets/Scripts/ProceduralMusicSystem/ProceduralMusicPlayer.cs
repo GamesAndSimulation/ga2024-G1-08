3572da3670e1c88be228ba90e5b6abb6246968b1
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-04-30 18:15:54 +0100

    Changed ProceduralMusicPlayer so waittime is defined by playtime

--- Assets/Scripts/ProceduralMusicSystem/ProceduralMusicPlayer.cs
+++ Assets/Scripts/ProceduralMusicSystem/ProceduralMusicPlayer.cs
@@ -12,12 +12,12 @@ public class ProceduralMusicPlayer : MonoBehaviour
 
     public ProceduralMusic musicToPlay;
 
-    private int nextParcelToPlayIndex; //the index of the next parcel to play (or that we're currently playing)
-    private int nextSoundToPlayIndex; //the index of the next sound to play in the next or current parcel
+    private int parcelPlayingIndex; //the index of the next parcel to play (or that we're currently playing)
+    private int soundPlayingNowIndex; //the index of the next sound to play in the next or current parcel
 
-    private ProceduralSound nextToPlay; //next sound to play
+    private ProceduralSound soundPlayingNow; //next sound to play
 
-    private float timeWaitingToPlay; //the time we already waited to play the next sound
+    private float timeToWait; //the time we need to wait to play the next note
     
 
 
@@ -27,24 +27,29 @@ public class ProceduralMusicPlayer : MonoBehaviour
 
     public void Start() {
 
-        nextParcelToPlayIndex = 0;
-        nextSoundToPlayIndex = 0;
-        switchToNextSound();
+        parcelPlayingIndex = 0;
+        soundPlayingNowIndex = 0;
 
     }
 
     public void Update() {
        
        //if we wait enough to play the next sound
-       if (timeFromMusic(musicToPlay.beatsPerMinute, nextToPlay.waitTime) <= timeWaitingToPlay) {
-       
-       
-           playSounds();
-       
-       
-       } else {
-       
-           timeWaitingToPlay += Time.deltaTime * 1000; //the miliseconds we're waiting
+       if (timeToWait <= 0) {
+
+            switchToNextSound();
+            channels[soundPlayingNow.channel].playSound(soundPlayingNow.note,
+                                            timeFromMusic(musicToPlay.beatsPerMinute, soundPlayingNow.getPlayTimeFrac()) - musicToPlay.noteIntervalDelayMili, //the time to play the music, the delay is used here
+                                            soundPlayingNow.fadeout);
+
+
+            timeToWait = timeFromMusic(musicToPlay.beatsPerMinute, soundPlayingNow.getPlayTimeFrac());
+
+
+
+        } else {
+
+            timeToWait -= Time.deltaTime * 1000; //the miliseconds we're waiting
        
        }
 
@@ -52,58 +57,31 @@ public class ProceduralMusicPlayer : MonoBehaviour
 
     private void switchToNextSound() {
 
-        nextToPlay = musicToPlay.musicParcels[nextParcelToPlayIndex].getSound(nextSoundToPlayIndex); //the first time this is called, the index is already 0
+        soundPlayingNowIndex++;
+        soundPlayingNow = musicToPlay.musicParcels[parcelPlayingIndex].getSound(soundPlayingNowIndex);
 
         //if this parcel does not have more sounds
-        if(nextToPlay == null) {
+        if(soundPlayingNow == null) {
 
             //switch to next parcel and reset soundToPlayIndex
-            nextSoundToPlayIndex = 0;
-            nextParcelToPlayIndex++;
+            soundPlayingNowIndex = 0;
+            parcelPlayingIndex++;
 
             //if there is no next parcel
-            if (nextParcelToPlayIndex >= musicToPlay.musicParcels.Count) {
+            if (parcelPlayingIndex >= musicToPlay.musicParcels.Count) {
 
                 if (!toRepeat)
                     gameObject.SetActive(false);
 
-                nextParcelToPlayIndex = 0;
+                parcelPlayingIndex = 0;
 
             }
 
-            nextToPlay = musicToPlay.musicParcels[nextParcelToPlayIndex].getSound(nextSoundToPlayIndex);
+            soundPlayingNow = musicToPlay.musicParcels[parcelPlayingIndex].getSound(soundPlayingNowIndex);
         
         }
 
     }
-
-    /**
-     * 
-     * Plays the next batch of sounds that are waiting
-     */
-    private void playSounds() {
-
-        //we already know that we can play the next sound
-        do {
-
-            Debug.Log("Playing sound at " + nextSoundToPlayIndex + " at batch " + nextParcelToPlayIndex);
-
-            channels[nextToPlay.channel].playSound(nextToPlay.note, 
-                timeFromMusic(musicToPlay.beatsPerMinute, nextToPlay.getPlayTimeFrac()) - musicToPlay.noteIntervalDelayMili, //the time to play the music, the delay is used here
-                nextToPlay.fadeout);
-
-            nextSoundToPlayIndex++;
-            timeWaitingToPlay = 0;
-
-            switchToNextSound();
-
-        
-            //this while is so if the next sound had a time of 0, we can play it in the same frame
-        } while (timeFromMusic(musicToPlay.beatsPerMinute, nextToPlay.waitTime) <= timeWaitingToPlay);
-
-  
-    }
-
 
 
     //calculates the actual time in miliseconds of a note
