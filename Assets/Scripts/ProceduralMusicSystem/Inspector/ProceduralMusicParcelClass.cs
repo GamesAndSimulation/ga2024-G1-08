8f917c4a2f8638cd689589429a65368fae7f19e6
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-04-30 15:55:03 +0100

    This gives this error:
    Fields with [SerializeReference] cannot serialize objects that derive from Unity.Object, in this case: Assembly-CSharp ProceduralMusicLoop

--- Assets/Scripts/ProceduralMusicSystem/Inspector/ProceduralMusicParcelClass.cs
+++ Assets/Scripts/ProceduralMusicSystem/Inspector/ProceduralMusicParcelClass.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [System.Serializable]
 public class ProceduralMusicParcelClass
 {
-        [SerializeField]
+        [SerializeReference, SerializeField]
         public ProceduralMusicParcel parcel;
 
         [SerializeField]
