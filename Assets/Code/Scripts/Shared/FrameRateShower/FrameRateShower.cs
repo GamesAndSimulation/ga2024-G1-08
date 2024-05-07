8bfde9eba829875136e8ab0bfba6b58694fa47db
RicardoGoncalo <r.goncalo@campus.fct.unl.pt>
2024-05-07 16:34:08 +0100

    Created frame rate shower and test scene for it

---
+++ Assets/Code/Scripts/Shared/FrameRateShower/FrameRateShower.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FrameRateShower : MonoBehaviour
+{
+
+    public float textRefreshRate = 0.1f;
+
+    public TextMeshProUGUI text;
+    private float currentFrameRate = 0;
+
+    private void OnEnable() {
+        StartCoroutine(refreshFrameRateText());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        currentFrameRate = (1 / Time.deltaTime);
+        
+    }
+
+    IEnumerator refreshFrameRateText() {
+
+        while (this.isActiveAndEnabled) {
+
+            text.text = currentFrameRate.ToString();
+
+            yield return new WaitForSeconds(textRefreshRate);
+
+        }
+
+    }
+
+}
