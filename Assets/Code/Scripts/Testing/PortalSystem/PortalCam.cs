b857fd423d341d844c6c28977fe34bfe50a7dfaf
André Tomás Ribeiro <at.ribeiro@campus.fct.unl.pt>
2024-05-20 16:20:49 +0100

    portal system essentially done
    -lacks player position alteration

---
+++ Assets/Code/Scripts/Testing/PortalSystem/PortalCam.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCam : MonoBehaviour
+{
+
+    public Transform playerCam;
+    public Transform thisPortal;
+    public Transform otherPortal;
+
+    void Update()
+    {
+        Vector3 playerOffset = playerCam.position - otherPortal.position;
+        transform.position = thisPortal.position + playerOffset;
+
+        float angularDifferenceBetweenPortalRotations = Quaternion.Angle(thisPortal.rotation, otherPortal.rotation);
+
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCam.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    
+    }
+}
---
+++ Assets/Code/Scripts/Testing/PortalSystem/PortalTeleport.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PortalTeleport : MonoBehaviour
+{
+    public Transform player;
+    public Transform receiver;
+
+    private bool enter;
+
+    private void Start()
+    {
+        enter = false;
+    }
+
+    void Update()
+    {
+        if (enter)
+        {
+            Vector3 portalToPlayer = player.position - transform.position;
+
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if(dotProduct < 0f)
+            {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+                Vector3 posOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + posOffset;
+                enter = false;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            enter = false;
+        }
+    }
+}
