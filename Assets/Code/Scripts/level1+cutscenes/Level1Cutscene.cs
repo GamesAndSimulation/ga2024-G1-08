50fdd93c6857b5929d2b71985071d6a1ea7fc09f
Mafalda Batalha <mf.batalha@campus.fct.unl.pt>
2024-06-05 01:13:35 +0100

    added some audio to level1 and adjusted some stuff like the dark room in the cabin

--- Assets/Code/Scripts/level1+cutscenes/Level1Cutscene.cs
+++ Assets/Code/Scripts/level1+cutscenes/Level1Cutscene.cs
@@ -10,8 +10,10 @@ public class Level1Cutscene : MonoBehaviour
     public FadeCanvas fadeCanvas;
     public Animator doorAnimator;
     public Animator cameraAnimator;
+    public AudioSource doorOpen;
 
     public const float WAITTIME_DOOROPEN = 1.0f;
+    public const float WAITTIME_DOORSOUND = 0.2f;
     public const float WAITTIME_CAMERAMOVE = 3.0f;
     public const float WAITTIME_CHANGETOPLAYER = 3.0f;
 
@@ -39,6 +41,9 @@ public class Level1Cutscene : MonoBehaviour
 
         yield return new WaitForSeconds(WAITTIME_DOOROPEN);
         doorAnimator.SetTrigger("doorOpen");
+
+        yield return new WaitForSeconds(WAITTIME_DOORSOUND);
+        doorOpen.Play();
 
     }
 
